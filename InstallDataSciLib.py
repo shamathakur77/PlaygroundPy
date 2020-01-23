349d729b712ae1be60120e6be0efbc5af52db470
Shama Thakur <shamathakur77@gmail.com>
2020-01-23 12:58:19 +0100

    Add files via upload

---
+++ InstallDataSciLib.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Install libraries
+
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns  # visualization tool
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+
+# In[ ]:
+
+
+
+
